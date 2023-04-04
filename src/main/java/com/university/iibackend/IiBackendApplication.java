679326cf7acaf262966f4111c77a851173e91e74
raulbaciulescu <raulbaciulescu@gmail.com>
2023-04-04 18:53:52 +0300

    first commit

---
+++ src/main/java/com/university/iibackend/IiBackendApplication.java
@@ -0,0 +1,13 @@
+package com.university.iibackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IiBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IiBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/university/iibackend/IiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.university.iibackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IiBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
