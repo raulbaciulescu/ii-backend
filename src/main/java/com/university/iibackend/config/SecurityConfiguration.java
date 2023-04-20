0a3ca8cbc719063aae276e21e4705ff396720b53
raulbaciulescu <raulbaciulescu@gmail.com>
2023-04-20 20:33:42 +0300

    cross origin

--- src/main/java/com/university/iibackend/config/SecurityConfiguration.java
+++ src/main/java/com/university/iibackend/config/SecurityConfiguration.java
@@ -10,6 +10,9 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -20,6 +23,14 @@ public class SecurityConfiguration {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.cors().configurationSource(request -> {
+            var cors = new CorsConfiguration();
+            cors.setAllowedOrigins(List.of("*"));
+            cors.setAllowedMethods(List.of("GET","POST", "PUT", "DELETE", "OPTIONS"));
+            cors.setAllowedHeaders(List.of("*"));
+            return cors;
+        });
+
         http
                 .csrf()
                 .disable()
--- src/main/java/com/university/iibackend/controller/DemoController.java
+++ src/main/java/com/university/iibackend/controller/DemoController.java
@@ -2,6 +2,7 @@ package com.university.iibackend.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/demo")
 @RequiredArgsConstructor
+@CrossOrigin
 public class DemoController {
     @GetMapping
     public ResponseEntity<String> sayHello() {
